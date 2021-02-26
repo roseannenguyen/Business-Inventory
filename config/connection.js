41f4c6b90b992c14dd2fdb524657b2e93294345f
Roseanne Nguyen <rtnguye1@gmail.com>
2021-02-26 08:56:35 -0700

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "AH#althycomputer1!",
+    password: "AH3althycomputer1!",
     database: "business_inventory"
 });
 
