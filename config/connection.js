e7fecae631657e1bdee13d95937d0adcafc1d704
Roseanne Nguyen <rtnguye1@gmail.com>
2021-02-26 09:33:12 -0700

    updated jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "AH3althycomputer1!",
-    database: "business_inventory"
-});
+var connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "AH3althycomputer1!",
+        database: "business_inventory"
+    });
+}
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
+connection.connect(function(err) {
+if (err) {
         console.error("error connecting: " + err.stack);
-        return;
+    return;
     }
     console.log("connected as id " + connection.threadId);
 });
