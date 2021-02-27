0b4718d4a964ce9550296965d5c7587f39d2a069
Roseanne Nguyen <rtnguye1@gmail.com>
2021-02-27 11:32:08 -0700

    added form and table on members.html

--- public/js/members.js
+++ public/js/members.js
@@ -1,63 +1,55 @@
-$(document).ready(() => {
-  // This file just does a GET request to figure out which user is logged in
+// This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   // $.get("/api/user_data").then(data => {
   //   $(".member-name").text(data.email);
   // });
 
-  $('[data-toggle="tooltip"]').tooltip();
-  var actions = $("table td:last-child").html();
-  // Append table with add row form on add new button click
-  $(".add-new").click(function () {
-    $(this).attr("disabled", "disabled");
-    var index = $("table tbody tr:last-child").index();
-    var row = '<tr>' +
-      '<td><input type="text" class="form-control" name="main-id" id="main-id"></td>' +
-      '<td><input type="text" class="form-control" name="item" id="item"></td>' +
-      '<td><input type="text" class="form-control" name="quantity" id="quantity"></td>' + 
-      '<td><input type="text" class="form-control" name="price" id="price"></td>' + 
-      '<td><input type="text" class="form-control" name="description" id="description"></td>' +
-      '<td>' + actions + '</td>' +
-      '</tr>';
-    $("table").append(row);
-    $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-  // Add row on add button click
-  $(document).on("click", ".add", function () {
-    var empty = false;
-    var input = $(this).parents("tr").find('input[type="text"]');
-    input.each(function () {
-      if (!$(this).val()) {
-        $(this).addClass("error");
-        empty = true;
-      } else {
-        $(this).removeClass("error");
-      }
-    });
-    $(this).parents("tr").find(".error").first().focus();
-    if (!empty) {
-      input.each(function () {
-        $(this).parent("td").html($(this).val());
-      });
-      $(this).parents("tr").find(".add, .edit").toggle();
-      $(".add-new").removeAttr("disabled");
-    }
-  });
-  // Edit row on edit button click
-  $(document).on("click", ".edit", function () {
-    $(this).parents("tr").find("td:not(:last-child)").each(function () {
-      $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
-    });
-    $(this).parents("tr").find(".add, .edit").toggle();
-    $(".add-new").attr("disabled", "disabled");
-  });
-  // Delete row on delete button click
-  $(document).on("click", ".delete", function () {
-    $(this).parents("tr").remove();
-    $(".add-new").removeAttr("disabled");
+$(document).ready(function () {
+
+  $('.close').on('click', function (e) {
+    e.preventDefault();
+    $(this).parent().parent().remove();
   });
 
+  $('#submit').click(function () {
+    var item = $('input[name=item]').val();
+    var quantity = $('input[name=quantity]').val();
+    var price = $('input[name=price]').val();
+    var description = $('input[name=description]').val();
+
+    var tr = "<tr>\
+                    <td>\
+                     <h2> " + item + "</h2>\
+                    </td>\
+                  <td>\
+                                    <h2> " + quantity+ "</h2>\
+                    </td>\
+                    <td>\
+                                    <h2> $" + price + "</h2>\
+                    </td>\
+                    \
+                    <td>\
+                       <button type=\"button\" class=\"close\"><span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">Close</span></button>\
+                        <textarea placeholder=\"Optional Description of Item\">" + description + "</textarea>\
+                    </td>\
+                </tr>;";
+    $('#table tbody').append(tr);
+
+    $("input[type=text]").val("");
+    $("input[type=number]").val("");
+
+  });
 });
 
+$(function () {
+  $('#orderModal').modal({
+    keyboard: true,
+    backdrop: "static",
+    show: false,
 
+  }).on('show', function () {
+    var getIdFromRow = $(event.target).closest('tr').data('id');
+    //make your ajax call populate items or what even you need
+    $(this).find('#orderDetails').html($('<b> Order Id selected: ' + getIdFromRow + '</b>'))
+  });
+});
