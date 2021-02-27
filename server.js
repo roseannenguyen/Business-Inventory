7e6478b02b77216cdfbd87df56e379b547f12820
Roseanne Nguyen <rtnguye1@gmail.com>
2021-02-26 19:50:01 -0700

    worked on html and added table in the members.html file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 // NEED ENVIRONMENT VARIABLE TO PROTECT THE CODE 
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({ secret: "funky business", resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
