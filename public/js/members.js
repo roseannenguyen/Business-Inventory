749ae65d640e5a622b3274a21c93426bfb4ab059
Roseanne Nguyen <rtnguye1@gmail.com>
2021-03-03 19:10:04 -0700

    removed some js to improve the table

--- public/js/members.js
+++ public/js/members.js
@@ -11,65 +11,16 @@ $.get("/api/user_data").then(data => {
 });
 
 submitBtn.on("click", event => {
-$.post("/api/items", {
-  name: itemName.val().trim(),
-  quantity: itemQuantity.val().trim(),
-  price: itemPrice.val().trim(),
-  body:itemDescription.val().trim()
-});
+  $.post("/api/items", {
+    name: itemName.val().trim(),
+    quantity: itemQuantity.val().trim(),
+    price: itemPrice.val().trim(),
+    body: itemDescription.val().trim()
+  });
 })
 
-// $(document).ready(function () {
 
-//   $('.close').on('click', function (e) {
-//     e.preventDefault();
-//     $(this).parent().parent().remove();
-//   });
-
-//   $('#submit').click(function () {
-//     var item = $('input[name=item]').val();
-//     var quantity = $('input[name=quantity]').val();
-//     var price = $('input[name=price]').val();
-//     var body = $('input[name=body]').val();
-
-//     var tr = "<tr>\
-//                     <td>\
-//                      <h2> " + item + "</h2>\
-//                     </td>\
-//                   <td>\
-//                                     <h2> " + quantity + "</h2>\
-//                     </td>\
-//                     <td>\
-//                                     <h2> $" + price + "</h2>\
-//                     </td>\
-//                     \
-//                     <td>\
-// <button type=\"button\" class=\"close\"><span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">Close</span></button>\
-// <textarea placeholder=\"Optional Description of Item\">" + body + "</textarea>\
-//                     </td>\
-//                 </tr>;";
-//     $('#table tbody').append(tr);
-
-//     $("input[type=text]").val("");
-//     $("input[type=number]").val("");
-
-//   });
-// });
-
-// $(function () {
-//   $('#orderModal').modal({
-//     keyboard: true,
-//     backdrop: "static",
-//     show: false,
-
-//   }).on('show', function () {
-//     var getIdFromRow = $(event.target).closest('tr').data('id');
-//     //make your ajax call populate items or what even you need
-//     $(this).find('#orderDetails').html($('<b> Order Id selected: ' + getIdFromRow + '</b>'))
-//   });
-// });
-
-window.onhashchange = function() {
+window.onhashchange = function () {
   update();
 };
 
